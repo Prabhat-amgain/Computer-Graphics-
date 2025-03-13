a8e4f81af470bd785df949a722168caf29d58c9c
Prabhat Amgain <amgainparbhat@gmail.com>
2025-03-13 09:28:43 +0545

    Add files via upload

---
+++ boundaryfill.cpp
@@ -0,0 +1,26 @@
+#include<graphics.h>
+
+void boundaryfill4(int x ,int y , int fill_color , int boundary_color)
+{
+	if(getpixel(x , y)!= boundary_color && getpixel(x , y)!=fill_color)
+	{
+		putpixel(x , y ,fill_color);
+		boundaryfill4(x+1 , y , fill_color,boundary_color);
+		boundaryfill4(x , y+1 , fill_color,boundary_color);
+		boundaryfill4(x-1 , y , fill_color,boundary_color);
+		boundaryfill4(x , y-1 , fill_color,boundary_color);
+	}
+}
+int main()
+{
+	int gd = DETECT , gm;
+	
+	initgraph(&gd, &gm ," ");
+	int x = 250, y=200 , radius =120;
+	circle(x , y, radius);
+	
+	boundaryfill4(x , y ,4, 15);
+	
+	delay(10000);
+	getch();
+}
---
+++ flood_fill_alg.cpp
@@ -0,0 +1,28 @@
+#include<graphics.h>
+#include<stdio.h>
+void flood(int x1 , int y1 , int new_col , int old_col)
+{
+	if(getpixel(x1 , y1)==old_col)
+	{
+		putpixel(x1 , y1 , new_col);
+		flood(x1+1,  y1 , new_col , old_col);
+		flood(x1-1,  y1 , new_col , old_col);
+		flood(x1,  y1+1 , new_col , old_col);
+		flood(x1,  y1-1 , new_col , old_col);
+	}
+}
+int main()
+{
+	int gd, gm = DETECT;
+	initgraph(&gd, &gm ," ");
+	rectangle(50 , 50 , 300 , 300);
+	int x1  = 150;
+	int y1 = 150;
+	
+	int newcolor = 4;
+	int oldcolor = 0;
+	flood(x1 , y1 , newcolor , oldcolor);
+	getch();
+	return 0;
+	
+}
